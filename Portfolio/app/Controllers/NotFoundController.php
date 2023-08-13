5dae124e730e6b29a966afff34d6a84364de7e94
sdarby1 <siensienderby@gmail.com>
2023-08-13 20:40:31 +0200

    change2

--- Portfolio/app/Controllers/NotFoundController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class NotFoundController extends BaseController {
-    public function index()
-    {
-        $this->response->view(statusCode: 404, path: '404');
-    }
-}
--- Portfolio/public/scripts/navbar.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.getElementById('hamburger-menu');
-const headerNav = document.getElementById('header-nav');
-
-hamburgerMenu.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('animate-bars');
-    headerNav.classList.toggle('animate-header-nav');
-});	
